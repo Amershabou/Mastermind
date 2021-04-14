4d331579f66a07ea97e690cd9a79b2dbc08269b3
Amer Shabou <amer.shabou@himarley.com>
2021-04-14 18:19:27 -0400

    Alot of changes to the code

--- pages/mastermind.js
+++ pages/mastermind.js
@@ -9,8 +9,8 @@ import React, {
   } from 'next/router'
   
   import styles from './styles.module.css'
-  import {Button } from "react-bootstrap";
-  import 'bootstrap/dist/css/bootstrap.css'
+  import {Button, Jumbotron, Alert } from "react-bootstrap";
+  import 'bootstrap/dist/css/bootstrap.css';
 
 
   
@@ -21,7 +21,10 @@ import React, {
       const [response, setResponse] = useState([]);
       const [responseHistory, setResponseHistory] = useState([]);
       const [messages, setMessages] = useState([]);
-      const [gusses, setGuesses] = useState([10]);
+      const [gusses, setGuesses] = useState(10);
+      const [count, setCount] = useState(0);
+      const [hasStarted, setHasStarted] = useState(false);
+      const [solved, setSolved] = useState(false);
   
       const isMounted = useRef(false);
       const res = useRef(["", "", "", ""]);
@@ -34,60 +37,68 @@ import React, {
           setRandom(number);
           res.current = ["", "", "", ""]
           setResponse(res.current);
-          setResponseHistory([]);
-          setGuesses([10]);
-          setNewGame(false)
+          setResponseHistory(Array(1).fill(res.current));
+          setGuesses(10);
+          setCount(0);
+          setNewGame(false);
+          setHasStarted(true);
+          setSolved(false);
+          setMessages([]);
           Array.from(document.querySelectorAll("input")).forEach(
             input => (input.value = "")
           );
-          console.log(random)
         } else {
           isMounted.current = true;
         }
-  
       }, [newGame])
   
   
-      const handleChange = (e) => {
-        let newNum = Number(e.target.value);
+      const handleClick = (e) => {
+        e.preventDefault();
         let id = e.target.id;
-        res.current[id] = newNum;
-        setResponse(res.current);
-        console.log(response)
-  
-  
+        console.log("id--->",typeof id)
+        if (res.current[res.current.length -1] !== ""){
+          return;
+        }
+        for (let i = 0; i<res.current.length; i++){
+          if(res.current[i] === ""){
+            res.current[i] = Number(id);
+            break;
+          }
+          setResponse(res.current)
+
+        }
       }
-  
       const checkIfMatch = (e) => {
         e.preventDefault();
         if (res.current.includes("")) return;
-        setResponseHistory(responseHistory => [...responseHistory, response]);
+        let newReponsehistory = responseHistory;
+        newReponsehistory[count] = response;
+        newReponsehistory.push(["","","",""])
+        setResponseHistory(newReponsehistory);
         setGuesses(gusses - 1)
+        setCount(count + 1);
         Array.from(document.querySelectorAll("input")).forEach(
           input => (input.value = "")
         );
         let newMessage = "";
-        console.log(response)
-        if (areEqual(random, response) === true) {
+        if (areEqual(random, res.current) === true) {
           newMessage = "Congratulations! If have gussed all the numbers and won the game!!!";
+          setSolved(true);
         } else {
           console.log(compare(random, response)[0], compare(random, response)[1])
           if (compare(random, response)[0]) {
-            newMessage = "You have guessed a current number and its current locartion";
+            newMessage = "Good try! You have guessed a current number and its current locartion";
           } else if (compare(random, response)[1]) {
-            newMessage = "You have guessed a current number";
+            newMessage = "Good try! You have guessed a current number";
           } else {
-            newMessage = `Sorry! Your guess isn't correct, you have ${gusses} guesses left`;
+            newMessage = `Oops! Your guess isn't correct. Try again!`;
           }
         }
-  
         setMessages(messages => [...messages, newMessage])
-        res.current = res.current.map(num => num = "")
-        setResponse(res.current)
-  
+        res.current = res.current.map(num => num = "")  
       }
       const areEqual = (random, response) => {
-        console.log(random.join("") === response.join(""))
         if (random.join("") === response.join("")) {
           return true;
         } else {
@@ -95,6 +106,7 @@ import React, {
         }
       }
       const compare = (num1, num2) => {
+        console.log("here.....",num1, num2)
         let samePos = false;
         let foundNum = false;
         for (let i = 0; i < num1.length; i++) {
@@ -112,28 +124,78 @@ import React, {
         return [samePos, foundNum]
       }
 
-    return (<div className={styles.game}>
-    {/* <button className={styles.button} onClick={()=>{setNewGame(true)}}>Start the Game</button> */}
-    <Button variant="primary" type="submit" size="lg" className={styles.button}  onClick={()=>{setNewGame(true)}}>
-    Start the Game
-    </Button>
-    <div className="help">
-        {gusses} guesses left
-      </div>
-    <div className={styles.body}>
+    return (<div className = {styles.container}>
+     {!hasStarted ?  
+     <Jumbotron className={styles.firstView} >
+       <h1 className={styles.item}> Mastermind Game!</h1>
+       <p className={styles.item}>
+       To start a new game, click on the start button below!
+       </p>
+    
+      <Button variant="primary"  size="lg" block className={styles.startButton} onClick={()=>{setNewGame(true)}}>
+       Start
+      </Button>
+   
+   </Jumbotron> 
+   
+   : solved ? 
+   <>
+      <Alert  variant="success" className={styles.solved}>
+        <Alert.Heading>Congratulations!</Alert.Heading>
+        <p>
+         You have gussed all the numbers and won the game!!! 
+         The number you guessed is {random}
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setHasStarted(false)} variant="outline-success">
+            Back to game!
+          </Button>
+        </div>
+      </Alert>
+    </>
+   
+   :<div className={styles.secondView}>
+    <h1 className={styles.item}> Mastermind Game!</h1>
+      <div className={styles.numbersTable}>
+       <p >
+         You have <b>{gusses} </b> Guesses Left
+       </p>
+
+       <div className={styles.box}>
         <form className={styles.numbersBox}>
-            {Array.from(Array(combination)).map((x, i) => <input  id ={i} key={i} onChange = {handleChange} className={styles.number} ></input>)}
+            {Array.from(Array(8)).map((x, i) => <button  id ={i} key={i} onClick = {handleClick} className={styles.number} >{i}</button>)}
 
         </form>
+     
+        <Button variant="success" className = {styles.guessSubmit} onClick = {checkIfMatch} >
+          Submit A Guess
+        </Button>
+      </div>
+     </div>
+    
+     <div className={styles.guessBox}>
+       {responseHistory.map((x, i) => 
+         <div className={styles.guessRow} id ={i} key={i} > 
+          <div className={styles.insideBox}> 
+            <div className={styles.text}>Attempt {i+1}</div>
+           {responseHistory[i].map((num, idx) => 
+             <button className={styles.square} id ={idx} key={idx}>
+               {num}
+             </button>)} 
+          </div>
+           <div className={styles.message}> 
+              {messages[i]}
+          </div>
+         </div>)}
 
-        <button className = {styles.guessSubmit} onClick = {checkIfMatch} >Submit Guess</button>
-    </div>
-    <div className={styles.body}>
-        <div className={styles.numbersBox}>
-            {responseHistory.map((x, i) => <div><div  id ={i} key={i} className={styles.number} >{x}</div> <div key={i}>{messages[i]}</div></div>)}
 
-        </div>
-    </div>
+     </div>
+     <Button variant="primary"  className={styles.newStartButton}  onClick={()=>{setNewGame(true)}}>
+       Start A New Game
+      </Button>
+    </div> }
+    
     </div>)
 }
 
